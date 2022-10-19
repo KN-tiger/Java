bdaced61d4ce7d53cdce09863b963da84b66fe41
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-10-19 09:33:33 +0000

    5章終了

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+
+  public static void main(String[] args) {
+    final int LUCKY_NUMBER = 16;
+        String str = "野口　佳奈代";
+        System.out.println(str);
+        str = "備えあれば患いなし";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+  }
+}
