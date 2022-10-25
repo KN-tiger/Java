962c4fa84f0ce9e5705eae5bb4ea71d8a5c41ba2
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-10-25 14:35:03 +0000

    基礎復習2

---
+++ Chap1_11.java
@@ -0,0 +1,12 @@
+public class Chap1_11 {
+  public static void main(String[] args) {
+    String num = "30";
+    System.out.println(num + 20);
+    System.out.println(
+     Integer.parseInt(num)+ 20);
+    int num2 = 30;
+    System.out.println( num2 + 20 );
+    System.out.println(
+      String.valueOf( num2 ) + 20 );
+  }
+}
