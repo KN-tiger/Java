2bf5b64907ba56eb9c3b3e8a3b71965c336e38fd
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-10-31 12:38:05 +0000

    基礎復習6

---
+++ Chap3_2.java
@@ -0,0 +1,9 @@
+public class Chap3_2 {
+  public static void main(String[] args) {
+    int money = 30000;
+    while( money >= 0 ){
+      System.out.println( money );
+      money = money - 5080;
+    }
+  }
+}
---
+++ Chap3_3.java
@@ -0,0 +1,8 @@
+public class Chap3_3 {
+  public static void main(String[] args) {
+    for(int count = 5; count >= 1; count--){
+      System.out.println(
+        count + "回目のハロー！");
+    }
+  }
+}
